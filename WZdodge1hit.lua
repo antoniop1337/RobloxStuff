57bb6edbd5a4b0e5f4f65bf4a77b785b95cecc3d
antoniop1337 <90808637+antoniop1337@users.noreply.github.com>
2022-10-23 11:43:02 +0100

    Create WZdodge1hit.lua

---
+++ WZdodge1hit.lua
@@ -0,0 +1,13 @@
+if not game:IsLoaded() then
+    game.Loaded:Wait()
+end
+game.Players.LocalPlayer.CharacterAdded:Wait()
+local workspace = game:GetService("Workspace")
+game.RunService.Heartbeat:Connect(function()
+    if workspace:FindFirstChild("RadialIndicator") then
+        local circle = workspace.RadialIndicator.Inner
+        if circle.Size.y >= 165 then
+            workspace.Mobs.BOSSFallenKing.Collider.CFrame = CFrame.new(workspace.MissionObjects.Spawn.Position)
+        end
+    end
+end)
